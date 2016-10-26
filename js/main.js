70592cc71f9c45bb732b1d40657b7816aae27046
Josefina <lonuz.j@gmail.com>
2016-10-25 21:50:13 -0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,54 @@
+
+var nombre = document.getElementById("name").value;
+var apellido = document.getElementById('lastname').value;
+var email = document.getElementById('input-email').value;
+var contrasena = document.getElementById('input-password').value;
+
 function validateForm(){
-	/* Escribe tú código aquí */
+	
+	function validarNombre(){
+
+		if( nombre == null || nombre.length == 0 || /^\s+$/.test(nombre) ) {
+	
+	alert("Este campo es obligatorio");  
+  	return false;
+
+		}
+	}
+	validarNombre();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+	if( apellido == null || valor.length == 0 || /^\s+$/.test(valor) ) {
+	
+	alert("Este campo es obligatorio");  
+  	return false;
+}
+	if( email == null || valor.length == 0 || /^\s+$/.test(valor) ) {
+	
+	alert("Este campo es obligatorio");  
+  	return false;
+}
+	if( contrasena == null || valor.length == 0 || /^\s+$/.test(valor) ) {
+	
+	alert("Este campo es obligatorio");  
+  	return false;
+}
+
+}
+*/
